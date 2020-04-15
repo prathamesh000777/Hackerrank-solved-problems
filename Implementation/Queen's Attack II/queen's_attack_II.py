d9f3c32c0bb17f1a53b7b82fe5727250b589d866
prathamesh vichare <51914475+prathamesh000777@users.noreply.github.com>
2020-04-15 23:03:36 +0530

    Contains code with explanation
    Modified the explanation for better understandability

--- Implementation/Queen's Attack II/queen's_attack_II.py
+++ Implementation/Queen's Attack II/queen's_attack_II.py
@@ -7,8 +7,8 @@ The first step is to check if the obstacle lies on which of the 4 possible way. 
 intersection of these ways is the position of the queen itself and as per the problem statement the obstacle can never be on the 
 queens position.
 
-Next step is to check if the obstacle is to the right or left of the queen, followed by closest element of right and left. And finally 
-calculate the possible ways to move by calculating the positions between queen and closest elements to right and left.
+Next step is to check if the obstacle is to the right or left of the queen on that line, followed by closest element of right and left.
+And finally calculate the possible ways to move by calculating the positions between queen and closest elements to right and left.
 
 If you have understood the approach till now you can skip the next part of the explanation where I explain the steps in detail.
 
@@ -20,19 +20,20 @@ For inclined lines we check if the line satisfies the condition y=mx+c where, y 
 Obstacle lies on the forward line (line with slope 1) if it satisfy the equation (row - column - intercept == 0)
 Obstacle lies on the backward line (line with slope 1) if it satisfy the equation (row + column - intercept == 0)
 
-For checking if the obstacle lies on the right or left of queen for horizontal,forward and backward line we only check the columns of
-the obstacle. For the vertical line we have to use row of obstacle for comparing.
+For checking if the obstacle lies on the right or left of the queen for each horizontal,forward and backward line, we can compare
+columns of the obstacle and queen. Whereas for the vertical line, we have to use row of obstacle for comparing.
 
-For example, suppose on a horizontal line queen is at 4th of the 8 column. Then for all the obstacle on the left (i.e. obstacles 
-with column 1,2,3) the closest one to the queen is with column=3. Hence we have used variable max_left to store this location. Similarly
-for right the closest one to the queen will be min_right (you can guess why :) ).
+For example, suppose on a horizontal line queen is at (4,4) with n = 8. Then for all the obstacle on the left (suppose (4,1) and (4,2))
+the closest one to the queen is with column=2. Hence we have used variable max_left to store the next free location i.e. max_left = 3.
+Similarly for right the closest one to the queen will be min_right (you can guess why :) ).
 
-For horizontal and vertical line we initiate the max_left and min_right just outside the chessboard i.e. 0 and n+1 respectively.
+For horizontal and vertical line we initiate the max_left and min_right to 1 and n respectively, where max_left and max_right are the
+variables that hold the leftmost and rightmost obstacle free position.
 
 For forward and backward line initiating the max_left and min_right is bit tricky.
-For forward line the max_left (c_q - min((r_q - 1),(c_q - 1)) - 1), where the term (min((r_q - 1),(c_q - 1))) gives the available blocks
-to the left of the queen on forward line. The terms checks which is minimum rows or columns to the left; as we can only move the
-minimum step only or we will be out of the chess board.
+For forward line the max_left (c_q - min((r_q - 1),(c_q - 1))), where the term (min((r_q - 1),(c_q - 1))) gives the available blocks
+to the left of the queen on forward line. The terms checks which is minimum rows or columns to the left; as we can only move these many
+steps or we will be out of the chess board.
 '''
 
 #!/bin/python3
@@ -46,17 +47,17 @@ import sys
 #Helper function
 def nearest_pos(i,queens_loc,max_left,min_right):
     #checks if the element is to the left(i < queens_loc) and if it is the closest to the left of queen by checking then update max_left
-    if (i < queens_loc) and (i > max_left): 
-        max_left = i
+    if (i < queens_loc) and (i >= max_left): 
+        max_left = i + 1
     #checks if the element is to the right(i > queens_loc) and if it is the closest to the right of queen then update min_right
-    elif (i > queens_loc) and (i < min_right):  
-        min_right = i
+    elif (i > queens_loc) and (i <= min_right):  
+        min_right = i - 1 
     return max_left,min_right
 
 #helper function
 def pos_count(max_left,min_right,queens_loc):
     #calculate the blocks available to move for the queen.
-    return (queens_loc - max_left - 1) + (min_right - queens_loc - 1)
+    return (queens_loc - max_left) + (min_right - queens_loc)
 
 # Complete the queensAttack function below.
 def queensAttack(n, k, r_q, c_q, obstacle):
@@ -64,15 +65,15 @@ def queensAttack(n, k, r_q, c_q, obstacle):
     fl_c = r_q - c_q      #forwardline y-intercept
     bl_c = r_q + c_q      #backwardline y-intercept
 
-    #obstacle position initiation
-    hl_max_left, hl_min_right = 0, n + 1    #explained on line 30
-    vl_max_bot, vl_min_top = 0, n + 1       #vertical line
+    #initiate the variables to hold leftmost and rightmost possible column till where queen can move
+    hl_max_left, hl_min_right = 1, n    #explained on line 30
+    vl_max_bot, vl_min_top = 1, n       #vertical line
 
-    #calculate the leftmost and rightmost possible column of the inclined lines
-    fl_max_left =  c_q - min((r_q - 1),(c_q - 1)) - 1   #this step is explained on line 32.
-    fl_min_right = c_q + min((n - r_q),(n - c_q)) + 1   #forwardline
-    bl_max_left = c_q - min((n - r_q),(c_q - 1)) - 1    #backwardline
-    bl_min_right = c_q +  min((r_q - 1),(n - c_q)) + 1
+    #calculate the leftmost and rightmost possible column of the inclined lines till where queen can move
+    fl_max_left =  c_q - min((r_q - 1),(c_q - 1))   #this step is explained on line 32.
+    fl_min_right = c_q + min((n - r_q),(n - c_q))   #forwardline
+    bl_max_left = c_q - min((n - r_q),(c_q - 1))    #backwardline
+    bl_min_right = c_q +  min((r_q - 1),(n - c_q))
 
 
     for [r_o,c_o] in obstacle:
@@ -90,7 +91,7 @@ def queensAttack(n, k, r_q, c_q, obstacle):
             bl_max_left, bl_min_right = nearest_pos(c_o, c_q, bl_max_left, bl_min_right)
     
     hl_count = pos_count(hl_max_left,hl_min_right, c_q)
-    vl_count = pos_count(vl_max_bot,vl_min_top, r_q)
+    vl_count = pos_count(vl_max_bot,vl_min_top, r_q)     # note that as we compared the row for queen we use r_q
     fl_count = pos_count(fl_max_left,fl_min_right, c_q)
     bl_count = pos_count(bl_max_left,bl_min_right, c_q)
     
